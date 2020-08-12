61da33dd18b29e3a04bfb56281a8105947888484
jennyw23 <jw10@wellesley.edu>
2020-08-12 13:58:14 -0400

    Completed 'Work with View Controllers' page

---
+++ FoodTracker/RatingControl.swift
@@ -0,0 +1,21 @@
+//
+//  RatingControl.swift
+//  FoodTracker
+//
+//  Created by Jenny Wang on 8/12/20.
+//  Copyright © 2020 Jenny Wang. All rights reserved.
+//
+
+import UIKit
+
+class RatingControl: UIStackView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
